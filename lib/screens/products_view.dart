61c86f8e22706761481b04dc62ec5d317fa1cabb
Yuri Toledo <yuriwtoledo@gmail.com>
2020-01-21 06:50:06 -0300

    Creation of Product View

---
+++ lib/screens/products_view.dart
@@ -0,0 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/datas/product.dart';
+import 'package:lojavirtual/screens/tiles/products_tiles.dart';
+
+class ProductsView extends StatelessWidget {
+  final DocumentSnapshot snapshot;
+
+  ProductsView(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot['title']),
+              centerTitle: true,
+              bottom: TabBar(
+                indicatorColor: Colors.white,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.grid_on)),
+                  Tab(
+                    icon: Icon(Icons.list),
+                  ),
+                ],
+              ),
+            ),
+            body: FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection('categories')
+                  .document(snapshot.documentID)
+                  .collection('items')
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  return TabBarView(children: <Widget>[
+                    GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisSpacing: 4.0,
+                          mainAxisSpacing: 4.0,
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.65),
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) {
+                        return ProductTile(
+                            'grid',
+                            ProductData.fromDocument(
+                                snapshot.data.documents[index]));
+                      },
+                    ),
+                    ListView.builder(
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) {
+                        return ProductTile(
+                            'list',
+                            ProductData.fromDocument(
+                                snapshot.data.documents[index]));
+                      },
+                    )
+                  ]);
+                }
+              },
+            )));
+  }
+}
