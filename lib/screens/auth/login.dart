b6c23b9a2f3b6cca9b54a7f8aef0fc37a85bca4a
Yuri Toledo <yuriwtoledo@gmail.com>
2020-03-04 06:59:33 -0300

    Adding SM and controllers in login

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -3,14 +3,55 @@ import 'package:lojavirtual/model/user_model.dart';
 import 'package:lojavirtual/screens/auth/register.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class LoginScreen extends StatelessWidget {
-  final formKey = GlobalKey<FormState>();
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   void _submit() {
-    final bool isValid = formKey.currentState.validate();
+    final bool isValid = _formKey.currentState.validate();
     if (!isValid) return;
 
-    ScopedModel.of<UserModel>(formKey.currentContext).signIn();
+    ScopedModel.of<UserModel>(context).signIn(
+        email: _emailController.text,
+        password: _passwordController.text,
+        onSuccess: onSuccessSubmit,
+        onFail: onFailSubmit);
+  }
+
+  void onSuccessSubmit() {
+    Navigator.of(context).pop();
+  }
+
+  void onFailSubmit() {
+    _showSnackbar(text: 'Erro! Verifique os dados e tente novamente');
+  }
+
+  void _showSnackbar({String text, bool isError = true}) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text(text),
+      backgroundColor:
+          isError ? Colors.redAccent : Theme.of(context).primaryColor,
+    ));
+  }
+
+  void _resetPassword() {
+    var email = _emailController.text;
+
+    if (email.isEmpty) {
+      _showSnackbar(text: 'Informe um email!');
+      return;
+    }
+    _showSnackbar(
+        text: 'Sucesso! Verique em sua caixa de entrada :)', isError: false);
+    ScopedModel.of<UserModel>(context).resetPassword(email);
   }
 
   @override
@@ -18,14 +59,15 @@ class LoginScreen extends StatelessWidget {
     final primaryColor = Theme.of(context).primaryColor;
 
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Login'),
         centerTitle: true,
         actions: <Widget>[
           FlatButton(
             onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => RegisterScreen()));
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (_) => RegisterScreen()));
             },
             child: Text(
               'CRIAR CONTA',
@@ -37,15 +79,16 @@ class LoginScreen extends StatelessWidget {
       body: ScopedModelDescendant<UserModel>(
         builder: (context, child, model) {
           if (model.isLoading) {
-            return CircularProgressIndicator();
+            return Center(child: CircularProgressIndicator());
           }
 
           return Form(
-            key: formKey,
+            key: _formKey,
             child: ListView(
               padding: EdgeInsets.symmetric(horizontal: 16),
               children: <Widget>[
                 TextFormField(
+                  controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
                   validator: (text) => text.isEmpty || !text.contains('@')
                       ? 'Email inválido'
@@ -55,6 +98,7 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
                 TextFormField(
+                  controller: _passwordController,
                   obscureText: true,
                   validator: (text) => text.isEmpty ? 'Senha inválida' : null,
                   decoration: InputDecoration(
@@ -64,9 +108,7 @@ class LoginScreen extends StatelessWidget {
                 Align(
                   alignment: Alignment.centerRight,
                   child: FlatButton(
-                    onPressed: () {
-                      model.signIn();
-                    },
+                    onPressed: _resetPassword,
                     child: Text(
                       'Esqueci minha senha',
                       style: TextStyle(color: primaryColor),
@@ -74,7 +116,7 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
                 RaisedButton(
-                  onPressed: _submit,
+                  onPressed: model.isLoading ? null : _submit,
                   child: Text(
                     'ACESSAR',
                     style: TextStyle(color: Colors.white),
