4dd4856036803ab08e7ce3f1d6bfb17d8ae6b96a
Yuri Toledo <yuriwtoledo@gmail.com>
2020-03-09 06:53:38 -0300

    addint toResumedMap()

--- lib/datas/product.dart
+++ lib/datas/product.dart
@@ -23,4 +23,12 @@ class ProductData {
     images = snapshot.data['images'];
     sizes = snapshot.data['sizes'];
   }
+
+  Map<String, dynamic> toResumedMap() {
+    return {
+      'title': title,
+      'description': description,
+      'price': price,
+    };
+  }
 }
