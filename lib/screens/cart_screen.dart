236857843b326fc83fd43be9e1fe52a52245c22e
Yuri Toledo <yuriwtoledo@gmail.com>
2020-03-16 06:59:23 -0300

    CartScreen creation

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/model/cart_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CARRINHO'),
+        centerTitle: true,
+        actions: <Widget>[
+          Container(
+              padding: EdgeInsets.only(right: 20),
+              child: Center(
+                child: ScopedModelDescendant<CartModel>(
+                  builder: (context, child, model) {
+                    final items = model.quantityItems();
+                    final pluralize = items > 1 ? 'itens' : 'item';
+                    return Text(
+                      '$items $pluralize',
+                      style: TextStyle(fontSize: 17),
+                    );
+                  },
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
